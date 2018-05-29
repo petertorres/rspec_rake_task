c286bba221018acee33edbccf68fd4279ac89882
Pete Torres <pete.torres@dice.com>
2018-05-29 11:03:29 -0500

    Move Rake file load outside of describe block.

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -1,22 +1,33 @@
 require 'spec_helper'
 require 'rake'
 
-describe ':spec namespace rake task' do
-  describe 'spec:turn_off_alarm' do
+load File.expand_path('../../Rakefile', __FILE__)
 
-    before do
-      load File.expand_path('../../Rakefile', __FILE__)
+describe 'Check Rake tasks in :spec namespace' do
+
+
+  describe 'TASK: :default' do
+    it 'should invoke the default spec task' do
+      Rake::Task['spec:default'].invoke
     end
+  end
 
+  describe 'TASK: Make Coffee' do
+    it 'should have task to make coffee' do
+      Rake::Task['spec:make_coffee'].invoke
+    end
+  end
+
+  describe 'TASK: Turn Off Alarm' do
     it 'should have task to turn off alarm' do
       Rake::Task['spec:turn_off_alarm'].invoke
     end
   end
 
-  describe 'spec:default' do
-
-    it 'should have a default task' do
-      Rake::Task['spec:default'].invoke
+  describe 'TASK: Groom Myself' do
+    it 'should have task to groom' do
+      Rake::Task['spec:groom_myself'].invoke
     end
   end
+
 end
